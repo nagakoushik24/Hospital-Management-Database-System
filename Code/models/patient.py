1baabff91328d88ab370e0ba55b848b82a934210
Naga Koushik <121690603+nagakoushik24@users.noreply.github.com>
2024-10-30 00:09:31 +0530

    Update patient.py

--- Code/models/patient.py
+++ Code/models/patient.py
@@ -8,10 +8,20 @@ def register_patient(name, age, gender, contact):
     conn.commit()
     conn.close()
 
+
 def get_patient_info(patient_id=None):
     conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM patients")
+    
+    if patient_id != '0':
+        cursor.execute('''
+            SELECT * FROM patients WHERE id = ?
+        ''', (patient_id,)) 
+    else:
+        cursor.execute('''
+            SELECT * FROM patients
+        ''')
+    
     patients = cursor.fetchall()
+    conn.close()
     return patients
-
