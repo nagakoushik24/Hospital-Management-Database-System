f63b13e55f08a5bade9b9a175a90a294311554bf
Naga Koushik <121690603+nagakoushik24@users.noreply.github.com>
2024-10-30 00:10:03 +0530

    Update room.py

--- Code/models/room.py
+++ Code/models/room.py
@@ -8,12 +8,19 @@ def assign_room(room_number, patient_id, status):
     conn.commit()
     conn.close()
 
-def get_rooms():
+def get_rooms(room_id=None):
     conn = connect_db()
     cursor = conn.cursor()
     
-    cursor.execute('SELECT * FROM rooms')
-
+    if room_id != '0':
+        cursor.execute('''
+            SELECT * FROM rooms WHERE room_id = ?
+        ''', (room_id,))
+    else:
+        cursor.execute('''
+            SELECT * FROM rooms
+        ''')
+    
     rooms = cursor.fetchall()
     conn.close()
-    return rooms  # Returns all rooms as a list of tuples
+    return rooms
